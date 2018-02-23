a8c1b940061ac44ec842b018cfb5b8d6dcfd0afd
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2018-02-23 18:15:39 -0300

    update project

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,21 +1,24 @@
-let deferredPrompt;
+
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
-    .then(() => {
-      console.log('service worker registered');
+    .then(function () {
+      console.log('Service worker registered!');
     })
-    .catch((err) => {
+    .catch(function(err) {
       console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('Before Install Prompt');
-
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
-
   return false;
 });
