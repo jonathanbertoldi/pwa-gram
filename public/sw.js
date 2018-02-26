7086856e708bf3740d34ee4618c282c6a771eadc
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2018-02-26 09:08:13 -0300

    cache

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,26 @@
-
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static').then((cache) => {
+      console.log('[Service Worker] Precaching App Shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
+      cache.add('/src/js/app.js');
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,5 +29,20 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.response)
+          .then((res) => {
+            return caches.open('dynamic').then((cache) => {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch((err) => {});
+      }
+    })
+  );
 });
